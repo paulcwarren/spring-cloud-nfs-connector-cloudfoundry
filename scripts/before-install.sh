ad335fb05fa668ca8848f4b9e3402641010a245c
Paul Warren <paul.warren@emc.com>
2017-11-15 22:19:25 -0800

    First attempt at setting up gpg signing

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-openssl aes-256-cbc -K $encrypted_2d46c2ddc73e_key -iv $encrypted_2d46c2ddc73e_iv -in codesigning.asc.enc -out codesigning.asc -d
-gpg --fast-import codesigning.asc
+#openssl aes-256-cbc -K $encrypted_2d46c2ddc73e_key -iv $encrypted_2d46c2ddc73e_iv -in codesigning.asc.enc -out codesigning.asc -d
+#gpg --fast-import codesigning.asc
+
+if [ ! -z "$GPG_SECRET_KEYS" ]; then echo $GPG_SECRET_KEYS | base64 --decode | gpg --import; fi
+if [ ! -z "$GPG_OWNERTRUST" ]; then echo $GPG_OWNERTRUST | base64 --decode | gpg --import-ownertrust; fi
 
 if [ -n "$TRAVIS_TAG" ]; then
     export BUILD_TYPE=release
