3985c900d32a0ec19ebd0bdb72f0ab4f7261b55f
Paul Warren <paul.warren@emc.com>
2017-11-15 22:04:34 -0800

    Prep for publishing to maven central

---
+++ scripts/before-install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+openssl aes-256-cbc -K $encrypted_2d46c2ddc73e_key -iv $encrypted_2d46c2ddc73e_iv -in codesigning.asc.enc -out codesigning.asc -d
+gpg --fast-import codesigning.asc
+
+if [ -n "$TRAVIS_TAG" ]; then
+    export BUILD_TYPE=release
+else
+    export BUILD_TYPE=snapshot
+fi
